88f64cc4f9cfc93bd55da0417a4d15b619d51739
Akhil Mohan <akhilerm@gmail.com>
2016-02-01 23:56:05 +0530

    Updated Python Script
    Now the python script generates thumbnails for videos in Content folder

--- conv.py
+++ conv.py
@@ -1,3 +1,24 @@
 import glob
-ls = glob.glob("*.*")
-print len(ls)
+import os
+import shutil
+
+def generator(video_files):
+	thumbs=glob.glob("Content\\thumbs\*.*")
+	for i in range(len(thumbs)):
+		thumbs[i]=(thumbs[i])[15:-4]
+	s1=set(thumbs)
+	s2=set(video_files)
+	if len(list(s1&s2)) == 0:
+		#thumbnail making start here
+		ls=list(s2-s1)
+		print ls
+		for i in range(len(ls)):	
+			os.system("copy Content\\thumbs\\sample.png Content\\thumbs\\"+ls[i]+".png")
+		
+		#thumbnail making ends here
+	return
+
+video_files=glob.glob("Content\*.*")
+for i in range(len(video_files)):
+	video_files[i]=(video_files[i])[8:-4]
+generator(video_files)
