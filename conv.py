ad12b27f5ac1036255465f7ad6299d1978242cba
Akhil Mohan <akhilerm@gmail.com>
2016-02-10 00:58:41 +0530

    Python script updated to generate video thumbnails
    python script now updated to generate video thumbnails of video in the
    Content/ folder

--- conv.py
+++ conv.py
@@ -1,23 +1,27 @@
 import glob
 import os
 import shutil
+import copy
 
 def generator(video_files):
+	temp=copy.copy(video_files)
+	for i in range(len(video_files)):
+		video_files[i]=(video_files[i])[8:-4]
 	thumbs=glob.glob("Content\\thumbs\\*.*")
 	for i in range(len(thumbs)):
 		thumbs[i]=(thumbs[i])[15:-4]
 	s1=set(thumbs)
 	s2=set(video_files)
-	if len(list(s1-s2)) != 0:
+	if len(list(s2-s1)) != 0:
 		#thumbnail making start here
 		ls=list(s2-s1)
 		for i in range(len(ls)):
-			os.system("copy Content\\thumbs\\sample.jpg Content\\thumbs\\"+ls[i]+".jpg")
-		
+			file=[s for s in temp if ls[i] in s]
+			os.system("ffmpeg -i "+file[0]+" -ss 00:00:14.435 -vframes 1 Content\\thumbs\\"+ls[i]+"_.jpg")
+			os.system("ffmpeg -i Content\\thumbs\\"+ls[i]+"_.jpg -vf scale=220:148 Content\\thumbs\\"+ls[i]+".jpg")
+			os.system("del Content\\thumbs\\"+ls[i]+"_.jpg")
 		#thumbnail making ends here
 	return
 
 video_files=glob.glob("Content/*.*")
-for i in range(len(video_files)):
-	video_files[i]=(video_files[i])[8:-4]
 generator(video_files)
--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <!DOCTYPE HTML>
 <html>
-<title>DEMO</title>
-<body>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Project1</title>
 
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
 <?php
-$video_dir = "/Content/";
+$a=0;
+$video_dir = "Content/";
+$thbdir="Content/thumbs/";
 $vids = glob($video_dir."*.*");
 foreach($vids as $vid)
 {	
-	echo "in loop";
-	$tmb = $video_dir."thumbs/".substr(basename($vid),0,-4).".jpg";
-    echo '<a href="'.basename($vid).'">'.basename($tmb).'></a>';
-	//echo '<img src="'.$tmb.'" width="50px" height="50px"/>';
-	echo '</br>';
+	if($a%4==0)
+		echo '<div class="row">';
+	echo $vid;	
+	echo '<div class="responsive-img col l3">';
+	$tmb=$thbdir.substr(basename($vid),0,-4).".jpg";
+	echo '<a href="'.basename($vid).'"><img class="abcd" src="'.$tmb.'"></img></a>';
+
+	echo '</div>';
+	if($a%4==3)
+		echo '</div>';
+	$a=$a+1;
 }
 ?>
+
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script src="js/init.js"></script>
 </body>
 </html>
